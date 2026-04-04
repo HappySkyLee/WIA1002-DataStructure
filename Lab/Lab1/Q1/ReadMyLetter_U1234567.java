7f81c36c56489a86aaa37465e4da7c15667443e3
HappySkyLee <happyskyleekh@gmail.com>
2026-04-04 23:18:23 +0800

    Lab1 Q1 Answer

--- Lab/Lab1/Q1/ReadMyLetter_U1234567.java
+++ Lab/Lab1/Q1/ReadMyLetter_U1234567.java
@@ -1,10 +1,12 @@
+package Q1;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadMyLetter_U1234567 {
     public static void main(String[] args) {
-        String filePath = "Lab/Lab1/HappySkyLee_U1234567.txt";
+        String filePath = "Lab/Lab1/Q1/HappySkyLee_U1234567.txt";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
